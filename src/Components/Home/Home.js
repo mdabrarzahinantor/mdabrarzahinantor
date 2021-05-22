cc37dd22a032e5fa9c301eb4153bcc5802532118
mdabrarzahinantor <mdabrarzahinantor2005@gmail.com>
2021-05-22 19:14:13 +0600

    init

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,29 +1,36 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./Home.css";
-import HamburgerAnimated from "../../Assets/Images/HamburgerAnimated.svg";
+import Bar from "../../Assets/Images/Bar.svg";
 import CrossMenu from "../../Assets/Images/CrossMenu.svg";
 import Logo from "../../Assets/Images/Logo.svg";
 import BannerIllustrationAnimated from "../../Assets/Images/BannerIllustrationAnimated.svg";
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+
+const variants = {
+  open: { opacity: 1, x: 0 },
+  closed: { opacity: 0, x: "100%" },
+};
 
 function Home() {
+  const [isOpen, setIsOpen] = useState(false);
+  const { ref, inView } = useInView();
+  const animation = useAnimation();
+
+  useEffect(() => {
+    if (inView) {
+      animation.start();
+    }
+  }, [inView]);
+
   return (
-    <div className="home">
+    <div ref={ref} className="home">
       <div className="scroll-to-top">
         <a href="#">
           <i class="fas fa-arrow-up    "></i>
         </a>
       </div>
-      <div className="mynav-hamburger">
-        <a
-          // onClick={() => {
-          //   document.querySelector(".mysidenav").classList.toggle("active");
-          // }}
-          id="toggler"
-          href="#"
-        >
-          <img src={HamburgerAnimated} />
-        </a>
-      </div>
+
       <div className="mynav">
         <div className="mynav-left">
           <a href="#">
@@ -53,37 +60,58 @@ function Home() {
           <a href="#projects">Projects</a>
         </div>
       </div>
-      <div style={{ display: "none" }} id="sidenav" className=" mysidenav">
-        <div className="mynav-crossmenu">
+      {/* Sidebar */}
+      <div>
+        <div className="mynav-hamburger">
           <a
-            id="toggler1"
             // onClick={() => {
             //   document.querySelector(".mysidenav").classList.toggle("active");
             // }}
+            id="toggler"
             href="#"
+            onClick={() => setIsOpen(!isOpen)}
           >
-            <img src={CrossMenu} />
+            <img src={Bar} />
           </a>
         </div>
+        <motion.div
+          animate={isOpen ? "open" : "closed"}
+          variants={variants}
+          id="sidenav"
+          className=" mysidenav"
+        >
+          <div className="mynav-crossmenu">
+            <a
+              onClick={() => setIsOpen(!isOpen)}
+              id="toggler1"
+              // onClick={() => {
+              //   document.querySelector(".mysidenav").classList.toggle("active");
+              // }}
+              href="#"
+            >
+              <img src={CrossMenu} />
+            </a>
+          </div>
 
-        <ul>
-          <li>
-            <i className="fas fa-tasks    "></i>
-            <a href="#projects">Projects</a>
-          </li>
-          <li>
-            <i className="fas fa-clock    "></i>
-            <a href="#services">Services</a>
-          </li>
-          <li>
-            <i className="fas fa-portrait    "></i>
-            <a href="#about">About</a>
-          </li>
-          <li>
-            <i class="fas fa-envelope    "></i>
-            <a href="#connect">Connect</a>
-          </li>
-        </ul>
+          <ul>
+            <li>
+              <i className="fas fa-tasks    "></i>
+              <a href="#projects">Projects</a>
+            </li>
+            <li>
+              <i className="fas fa-clock    "></i>
+              <a href="#services">Services</a>
+            </li>
+            <li>
+              <i className="fas fa-portrait    "></i>
+              <a href="#about">About</a>
+            </li>
+            <li>
+              <i class="fas fa-envelope    "></i>
+              <a href="#connect">Connect</a>
+            </li>
+          </ul>
+        </motion.div>
       </div>
       <div className="banner">
         <div className="banner-left">
