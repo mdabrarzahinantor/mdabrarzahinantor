7d29f9ee0c5c82702c6d9ad2dcde399d968f4ccb
mdabrarzahinantor <mdabrarzahinantor2005@gmail.com>
2021-05-23 09:41:02 +0600

    init

--- src/Components/Services/Services.js
+++ src/Components/Services/Services.js
@@ -10,7 +10,7 @@ function Services() {
         <div
           data-aos="fade-in"
           data-aos-duration="1000"
-          data-aos-delay="200"
+          data-aos-delay="300"
           data-aos-easing="ease-in-out-quad"
           className="spcl"
         >
--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -16,7 +16,7 @@ function Skills() {
       <div
         data-aos="fade-in"
         data-aos-duration="1000"
-        data-aos-delay="200"
+        data-aos-delay="300"
         data-aos-easing="ease-in-out-quad"
         className=" spcl"
       >
